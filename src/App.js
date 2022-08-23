0cabbb2532a19b3a3271d911db338954024393dd
vikram <ettevikram2010@gmail.com>
2022-08-23 09:12:10 +0530

    adding one more heading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>This is Vikram, Creating localRepository and pushing to globalRepository via git push</h1>
       <h1>This is Continuous Integration</h1>
       <h1>Iam learnig Continuous Integration today</h1>
     </div>
