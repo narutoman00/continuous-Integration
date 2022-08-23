868a654d4fa3ce49fe33d3b0fa10fe9a993cd801
vikram <ettevikram2010@gmail.com>
2022-08-23 10:13:12 +0530

    added last heading

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <h1>This is Continuous Integration</h1>
       <h1>change the npm run build command so that netlify understood that warning as warnig and error is error while fetching code</h1>
       <h1>Iam learnig Continuous Integration today</h1>
+      <h1>I have deployed this code in netlify</h1>
     </div>
   );
 }
